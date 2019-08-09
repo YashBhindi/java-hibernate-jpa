7479026d97325d075268e33bfe531a630dbd4d79
Yash <ybhindi@argusoft.in>
2019-08-09 13:05:47 +0530

    Fetch and insert of messages with the help of simple JPA done

---
+++ src/main/java/com/java/jpa/demo/dao/HelloWorldDao.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.java.jpa.demo.dao;
+
+import com.java.jpa.demo.model.HelloWorld;
+import java.util.List;
+import java.util.Objects;
+import java.util.Scanner;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author yash
+ */
+public class HelloWorldDao {
+
+    EntityManagerFactory entityManagerFactory;
+    EntityManager entityManager;
+    EntityTransaction entityTransaction;
+    Scanner sc;
+
+    public HelloWorldDao() {
+        entityManagerFactory = Persistence.createEntityManagerFactory("per");
+        entityManager = entityManagerFactory.createEntityManager();
+        entityTransaction = entityManager.getTransaction();
+        entityTransaction.begin();
+        sc = new Scanner(System.in);
+    }
+
+    public void addMessage() {
+        String msg = new String();
+        System.out.println("Enter message:");
+        msg = sc.next();
+        if (Objects.isNull(msg)) {
+            System.out.println("Provide valid message..");
+        } else {
+            HelloWorld helloWorld = new HelloWorld(msg);
+            entityManager.persist(helloWorld);
+            entityManager.getTransaction().commit();
+            System.out.println("Message added..");
+        }
+    }
+
+    public void getAllMessages() {
+        //retrieve data'
+        Query query = entityManager.createQuery("SELECT m FROM HelloWorld m", HelloWorld.class);
+        List<HelloWorld> message = query.getResultList();
+        for (HelloWorld m : message) {
+            System.out.println("Message:" + m.getMessage());
+        }
+
+    }
+
+    public void closeConnection() {
+        entityManager.close();
+        entityManagerFactory.close();
+    }
+
+}
---
+++ src/main/java/com/java/jpa/demo/model/UserProfile.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.java.jpa.demo.model;
+
+/**
+ *
+ * @author yash
+ */
+import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+
+@Entity(name = "UserProfile")
+@Table(name = "user_profile")
+public class UserProfile {
+
+//    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @EmbeddedId
+    private CompositeKey compositeKey;
+
+    @Column(name = "email")
+    private String email;
+
+    public UserProfile(CompositeKey compositeKey, String email) {
+        this.compositeKey = compositeKey;
+        this.email = email;
+    }
+
+    public UserProfile() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public CompositeKey getCompositeKey() {
+        return compositeKey;
+    }
+
+    public void setCompositeKey(CompositeKey compositeKey) {
+        this.compositeKey = compositeKey;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return ReflectionToStringBuilder.toString(this);
+    }
+
+}
